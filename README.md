394568dc39fdf0772a2f654134c8bb674a42b85f
Troy Calandra <troydcalandra@gmail.com>
2021-06-20 20:39:18 -0400

    Adds script for sales flow

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # An Arcade NFT
+
+**Set Up Truffle Config With Multiple Accounts**
+
+1. Create MetaMask account
+2. Retrieve mnemonic phrase
+3. Enter mnemonic phrase in place of `MNEMONIC_PHRASE` in `truffle-config.js`
+
+**Run Purchase Flow Script**
+
+`$ truffle exec ./sales-flow.js --network ropsten`
---
+++ sales-flow.js
@@ -0,0 +1,18 @@
+module.exports = async function(callback) {
+  const accounts = await web3.eth.getAccounts();
+  const sale = await artifacts.require("Sales").deployed();
+  const arcade = await artifacts.require("Arcade").deployed();
+  
+  const salesBal = await web3.eth.getBalance(sale.address);
+  const totalSupply = await arcade.totalSupply();
+  console.log('New Token ID: ', totalSupply.words[0])
+  await arcade.mint(accounts[0],totalSupply.words[0]);
+  console.log(`Token ID ${totalSupply.words[0]} assigned to account ${await arcade.ownerOf(totalSupply.words[0])}`);
+  await arcade.setApprovalForAll(sale.address,true);
+  console.log(`Approval assigned to ${sale.address} from ${accounts[0]}`)
+  await sale.purchaseToken(totalSupply.words[0], {value: 200, from: accounts[1]});
+  console.log(`${await arcade.ownerOf(totalSupply.words[0]) === accounts[1] ? 'Success!' : 'Failure!'} Token ID ${totalSupply.words[0]} was purchased by account ${await arcade.ownerOf(totalSupply.words[0])}`);
+  const newSalesBal = await web3.eth.getBalance(sale.address);
+  console.log(`${salesBal <= newSalesBal ? 'Success!' : 'Failure!'} Sales Contract balance updated from ${salesBal} to ${newSalesBal}`);
+  callback();
+}
